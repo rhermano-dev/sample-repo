a8a8cb2c5e90e9c0effb4b0e3fcb17fca6e80a41
rhermano-pc <reynaldohermano3@gmail.com>
2026-06-15 16:49:26 +0800

    commit

---
+++ WebApplication1/WebApplication1/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public static List<PersonModel> listOfPerson = new();
+        public IActionResult Index()
+        {
+            return View(listOfPerson);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(PersonModel p)
+        {
+            p.Id = Guid.NewGuid().ToString();
+            listOfPerson.Add(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
